e35dd8d78d4d0782dff7c0cd8a36084d539d8582
tommy <tcatty7@gmail.com>
2023-11-09 17:12:54 -0500

    made the repeat word button

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-let speak = document.getElementById('speak')
-let check = document.getElementById('check')
-let textField = document.getElementById('text-field')
+let speak = document.getElementById('speak');
+let check = document.getElementById('check');
+let textField = document.getElementById('text-field');
+let number = 0;
 
 //fixing ai voice bug
 window.onload = function() {
@@ -8,25 +9,39 @@ window.onload = function() {
     window.speechSynthesis.speak(utterance);
         } 
 
- //repeats the random word from database
-    function Random() {
-        let word = wordList[Math.floor(Math.random()*wordList.length)];
-        let voices = window.speechSynthesis.getVoices();
-        let utterance = new SpeechSynthesisUtterance(word);
-        let desiredVoice = voices.find(function (voice) {
-           return voice.name === "Google UK English Male";
-         });
-         utterance.voice = desiredVoice;
-         utterance.rate = .9
-         window.speechSynthesis.speak(utterance);
-         console.log(voices)
-    }
-
-speak.addEventListener('click', () => {
-    Random()
+ //says a random word the from database
+    let clicked = false;
+    check.addEventListener('click', () => {
+    textField.value = '';
+    clicked = true
+    number++
+    let word = wordList[Math.floor(Math.random()*wordList.length)];
+    let voices = window.speechSynthesis.getVoices();
+    localStorage.setItem('word' + number, word);
+    let a = localStorage.getItem('word' + number);
+    let utterance = new SpeechSynthesisUtterance(a);
+    let desiredVoice = voices.find(function (voice) {
+       return voice.name === "Google UK English Male";
+     });
+     utterance.voice = desiredVoice;
+     utterance.rate = .9;
+     window.speechSynthesis.speak(utterance);
 })
 
-check.addEventListener('click', () => {
-textField.value = '';
-Random()
+//repeats the random word from database
+speak.addEventListener('click', () => {
+    if(clicked === false){
+        return;
+    }
+    let a = localStorage.getItem('word' + number);
+    let voices = window.speechSynthesis.getVoices();
+    let utterance = new SpeechSynthesisUtterance(a);
+    let desiredVoice = voices.find(function (voice) {
+       return voice.name === "Google UK English Male";
+     });
+     utterance.voice = desiredVoice;
+     utterance.rate = .9;
+    window.speechSynthesis.speak(utterance);
+
+   
 })
