cf891bb6e04e913d6af39b672ea76a5b58df523f
tommy <tcatty7@gmail.com>
2023-11-09 14:47:04 -0500

    working on vocies

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
-let check = document.getElementById('check')
+let speak = document.getElementById('speak')
 
 
-check.addEventListener('click', () => {
-    var word = wordList[Math.floor(Math.random()*wordList.length)];
-    document.getElementById("example").innerText = word;
+speak.addEventListener('click', () => {
+   let word = wordList[Math.floor(Math.random()*wordList.length)];
+   let voices = window.speechSynthesis.getVoices();
+   let utterance = new SpeechSynthesisUtterance(word);
+   let desiredVoice = voices.find(function (voice) {
+      return voice.name === "Google UK English Male";
+    });
+    utterance.voice = desiredVoice;
+    utterance.rate = .8
+    window.speechSynthesis.speak(utterance);
+    console.log(voices)
 })
