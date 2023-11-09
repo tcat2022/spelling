33cb5dfc85e14622a5b23c1bed1f9befd446de95
tommy <tcatty7@gmail.com>
2023-11-09 16:08:35 -0500

    check

--- script.js
+++ script.js
@@ -1,26 +1,27 @@
 let speak = document.getElementById('speak')
-
+let check = document.getElementById('check')
+let textField = document.getElementById('text-field')
+//fixing ai voice bug
 window.onload = function() {
-    let voices = window.speechSynthesis.getVoices();
-    let utterance = new SpeechSynthesisUtterance('test');
-    let desiredVoice = voices.find(function (voice) {
-       return voice.name === "Google UK English Male";
-     });
-     utterance.voice = desiredVoice;
-     utterance.rate = .9
-     window.speechSynthesis.speak(utterance);
-     console.log(voices)
-  };
-
+    Random()
+        } 
+    function Random() {
+        let word = wordList[Math.floor(Math.random()*wordList.length)];
+        let voices = window.speechSynthesis.getVoices();
+        let utterance = new SpeechSynthesisUtterance(word);
+        let desiredVoice = voices.find(function (voice) {
+           return voice.name === "Google UK English Male";
+         });
+         utterance.voice = desiredVoice;
+         utterance.rate = .9
+         window.speechSynthesis.speak(utterance);
+         console.log(voices)
+    }
+  //repeats the random word from database
 speak.addEventListener('click', () => {
-   let word = wordList[Math.floor(Math.random()*wordList.length)];
-   let voices = window.speechSynthesis.getVoices();
-   let utterance = new SpeechSynthesisUtterance(word);
-   let desiredVoice = voices.find(function (voice) {
-      return voice.name === "Google UK English Male";
-    });
-    utterance.voice = desiredVoice;
-    utterance.rate = .9
-    window.speechSynthesis.speak(utterance);
-    console.log(voices)
+    Random()
+})
+check.addEventListener('click', () => {
+textField.value = '';
+Random()
 })
