67aa7ecd08ed71cb8ca5da046f7faec0fce14d2c
tommy <tcatty7@gmail.com>
2023-11-09 19:50:44 -0500

    done with js yay!!!!

--- script.js
+++ script.js
@@ -2,14 +2,25 @@ let speak = document.getElementById('speak');
 let check = document.getElementById('check');
 let textField = document.getElementById('text-field');
 let number = 0;
-let counter = false;
+let counter = true;
 let score = 0
 let scoreField  = document.getElementById('score').innerText = score;
 
 //fixing ai voice bug
 window.onload = function() {
-
+    let word = wordList[Math.floor(Math.random()*wordList.length)];
+    let voices = window.speechSynthesis.getVoices();
+    localStorage.setItem('word' + number, word);
+    let a = localStorage.getItem('word' + number);
+    let utterance = new SpeechSynthesisUtterance(a);
+    let desiredVoice = voices.find(function (voice) {
+       return voice.name === "Google UK English Male";
+     });
+     utterance.voice = desiredVoice;
+     utterance.rate = .9;
+     window.speechSynthesis.speak(utterance);
         } 
+
 
  //says a random word the from database
     check.addEventListener('click', () => {
@@ -34,10 +45,11 @@ window.onload = function() {
                 counter = false
              }
     },1)   
-    if(counter == true){
-        score++
+    if(counter != true){
+        score += 1
     }
-    scoreField  = document.getElementById('score').innerText = score;
+    scoreField  = document.getElementById('score').innerText ='missed ' + score + ' out of ' + number;
+    console.log(score)
      textField.value = '';
 })
 
