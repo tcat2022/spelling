82fecbe64bae69cd88baa44d9485c77a9414f2d8
tommy <tcatty7@gmail.com>
2023-11-09 19:28:01 -0500

    done all core mechanics

--- script.js
+++ script.js
@@ -2,20 +2,15 @@ let speak = document.getElementById('speak');
 let check = document.getElementById('check');
 let textField = document.getElementById('text-field');
 let number = 0;
+let counter = false;
+let score = 0
+let scoreField  = document.getElementById('score').innerText = score;
 
 //fixing ai voice bug
 window.onload = function() {
-    let utterance = new SpeechSynthesisUtterance('');
-    window.speechSynthesis.speak(utterance);
-        } 
-
- //says a random word the from database
-    let clicked = false;
-    check.addEventListener('click', () => {
-    textField.value = '';
-    clicked = true
     number++
     let word = wordList[Math.floor(Math.random()*wordList.length)];
+
     let voices = window.speechSynthesis.getVoices();
     localStorage.setItem('word' + number, word);
     let a = localStorage.getItem('word' + number);
@@ -26,13 +21,62 @@ window.onload = function() {
      utterance.voice = desiredVoice;
      utterance.rate = .9;
      window.speechSynthesis.speak(utterance);
+     let value;
+     setInterval(function() {
+        value = textField.value
+        if(value === a){
+            counter = true;
+             }else{
+                counter = false
+             }
+             scoreField  = document.getElementById('score').innerText = score;
+    },1)   
+    if(counter == true){
+        score++
+    }
+    
+     textField.value = '';
+        } 
+
+
+
+
+ //says a random word the from database
+    check.addEventListener('click', () => {
+    number++
+    let word = wordList[Math.floor(Math.random()*wordList.length)];
+
+    let voices = window.speechSynthesis.getVoices();
+    localStorage.setItem('word' + number, word);
+    let a = localStorage.getItem('word' + number);
+    let utterance = new SpeechSynthesisUtterance(a);
+    let desiredVoice = voices.find(function (voice) {
+       return voice.name === "Google UK English Male";
+     });
+     utterance.voice = desiredVoice;
+     utterance.rate = .9;
+     window.speechSynthesis.speak(utterance);
+     let value;
+     setInterval(function() {
+        value = textField.value
+        if(value === a){
+            counter = true;
+             }else{
+                counter = false
+             }
+             scoreField  = document.getElementById('score').innerText = score;
+    },1)   
+    if(counter == true){
+        score++
+    }
+    
+     textField.value = '';
 })
+
+
 
 //repeats the random word from database
 speak.addEventListener('click', () => {
-    if(clicked === false){
-        return;
-    }
     let a = localStorage.getItem('word' + number);
     let voices = window.speechSynthesis.getVoices();
     let utterance = new SpeechSynthesisUtterance(a);
