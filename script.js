6b631aad98a8ee9a6e1e138b04b478887cb1eb7e
tommy <tcatty7@gmail.com>
2023-12-14 18:36:59 -0500

    whoops fixed enter error

--- script.js
+++ script.js
@@ -100,39 +100,56 @@ document.body.addEventListener('click', function(){
 
 textField.addEventListener('keypress', (event) => {
   if (event.keyCode == 13) {
-  number++
-  completed++
-  let word = wordList[Math.floor(Math.random()*wordList.length)];
-  let voices = window.speechSynthesis.getVoices();
-  localStorage.setItem('word' + number, word);
-  let a = localStorage.getItem('word' + number);
-  let index = wordList.indexOf(word);
-  wordList.splice(index,1);
-  let utterance = new SpeechSynthesisUtterance(a);
-  let desiredVoice = voices.find(function (voice) {
-     return voice.name === "Google US English";
-   });
-   utterance.voice = desiredVoice;
-   utterance.rate = .9;
-   window.speechSynthesis.speak(utterance);
-  if(counter != true){
-      score += 1
-    var p = document.createElement('p');
-    p.textContent = number - 1 + '. ' + localStorage.getItem('word' + `${number - 1}`);
-    missed.appendChild(p);
-    p.classList.add('wrong')
-    p.scrollIntoView();
-  }else {
-    var p1 = document.createElement('p');
-    p1.textContent = number - 1 + '. ' +  localStorage.getItem('word' + `${number - 1}`);
-    missed.appendChild(p1);
-    p1.classList.add('correct')
-    p1.scrollIntoView();
-  }
-  let percentage = Math.floor(score/completed*100)
-  right.innerText = `${100 - percentage}` + '%' + ' Right'
-  scoreField  = document.getElementById('score').innerText ='Missed ' + score + ' out of ' + `${number - 1} `;
-textField.value = '';
+    number++
+    completed++
+    let word = wordList[Math.floor(Math.random()*wordList.length)];
+    let voices = window.speechSynthesis.getVoices();
+    localStorage.setItem('word' + number, word);
+    let a = localStorage.getItem('word' + number);
+    let index = wordList.indexOf(word);
+    wordList.splice(index,1);
+    console.log(index)
+   let utterance = new SpeechSynthesisUtterance(a);
+    let desiredVoice = voices.find(function (voice) {
+       return voice.name === "Google US English";
+     });
+     utterance.voice = desiredVoice;
+     utterance.rate = .9;
+     window.speechSynthesis.speak(utterance);
+
+    if(counter != true){
+        score += 1
+      var p = document.createElement('p');
+      var p1 = document.createElement('p');
+      var p2 = document.createElement('p');
+      p.textContent = number - 1 + '. ';
+      p2.textContent =localStorage.getItem('word' + `${number - 1}`);
+      p1.textContent = textField.value
+      p1.classList.add('wrong')
+      p.classList.add('black')
+      p2.classList.add('correct')
+      let div = document.createElement('div');
+      div.classList.add('div')
+      div.append(p,p1,p2)
+      missed.append(div);
+      p2.scrollIntoView(); 
+    }else {
+      var correct = document.createElement('p');
+      var number1 = document.createElement('p');
+      correct.textContent =localStorage.getItem('word' + `${number - 1}`);
+      number1.textContent = number - 1 + '. ';
+      let div = document.createElement('div');
+      div.classList.add('div')
+      div.append(number1,correct)
+      missed.append(div);
+      number1.classList.add('black')
+      correct.classList.add('correct')
+      correct.scrollIntoView();
+    }
+    let percentage = Math.floor(score/completed*100)
+    right.innerText = `${100 - percentage}` + '%' + ' Right'
+    scoreField  = document.getElementById('score').innerText ='Missed ' + score + ' out of ' + `${number - 1} `;
+     textField.value = '';    
 }
 })
 
